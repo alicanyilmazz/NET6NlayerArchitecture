6b4f5d7ca4675f548573ea680939d04a829d5a8a
alican yilmaz <alicanyilmaz101@gmail.com>
2022-02-15 15:26:34 +0300

    IService created.

---
+++ NLayer.Core/Services/IService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Services
+{
+    public interface IService<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+        Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>> expression);
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task<T> AddAsync(T entity);
+        Task Update(T entity);
+        Task Remove(T entity);
+        Task RemoveRange(IEnumerable<T> entities);
+    }
+}
