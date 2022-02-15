af7b55fe42e40e9e28124b1fc03ccd4de3ce70fc
alican yilmaz <alicanyilmaz101@gmail.com>
2022-02-15 17:20:15 +0300

    ? added to ProductFeature for null safety.

--- NLayer.Core/ProductFeature.cs
+++ NLayer.Core/ProductFeature.cs
@@ -9,10 +9,10 @@ namespace NLayer.Core
     public class ProductFeature
     {
         public int Id { get; set; }
-        public string color { get; set; }
+        public string? Color { get; set; }
         public int Height { get; set; }
         public int Width { get; set; }
         public int ProductId { get; set; }
-        public Product Product { get; set; }
+        public Product? Product { get; set; }
     }
 }
