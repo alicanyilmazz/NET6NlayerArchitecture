c84e89499c515b5b35bf9912f1b53ca241cd3e50
alican yilmaz <alicanyilmaz101@gmail.com>
2022-02-15 17:34:31 +0300

    AppDbContext created.

---
+++ NLayer.Repository/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core;
+
+namespace NLayer.Repository
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet<Product>? Products { get; set; }
+        public DbSet<ProductFeature>? ProductFeatures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Category>().HasKey(x => x.Id).HasName("Category");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
+
