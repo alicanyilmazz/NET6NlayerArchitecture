99125fa8688c6877c2e07ad71cb31e082c969475
alican yilmaz <alicanyilmaz101@gmail.com>
2022-02-19 17:28:26 +0300

    GenericRepository created.

---
+++ NLayer.Repository/Repositories/GenericRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NLayer.Core.Repositories;
+
+namespace NLayer.Repository.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        protected readonly AppDbContext Context;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public GenericRepository(AppDbContext context)
+        {
+            Context = context;
+            _dbSet = Context.Set<TEntity>();
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task AddRangeAsync(IEnumerable<TEntity> entities)
+        {
+            await _dbSet.AddRangeAsync(entities);
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<TEntity, bool>> expression)
+        {
+            return await _dbSet.AnyAsync(expression);
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return _dbSet.AsNoTracking().AsQueryable();
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _dbSet.Remove(entity); // _context.Entry(entity).State = EntityState.Deleted; // both of two are same thing.
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> expression)
+        {
+            return _dbSet.Where(expression);
+        }
+    }
+}
