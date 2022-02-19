e290a2e59ca46f89bba659aa4b56ec185198dc24
alican yilmaz <alicanyilmaz101@gmail.com>
2022-02-19 17:28:49 +0300

    Seeds created for initial data.

---
+++ NLayer.Repository/Seeds/CategorySeed.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core.DTOs.Model;
+
+namespace NLayer.Repository.Seeds
+{
+    internal class CategorySeed : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasData(
+                new Category() { Id = 1, Name = "Electronics" },
+                new Category() { Id = 2, Name = "Books" },
+                new Category() { Id = 3, Name = "Furniture" }
+                );
+        }
+    }
+}
---
+++ NLayer.Repository/Seeds/ProductSeed.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core.DTOs.Model;
+
+namespace NLayer.Repository.Seeds
+{
+    internal class ProductSeed : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData(
+                new Product() {Id = 1,CategoryId = 1,Name = "HeadPhone",Price = 100,Stock = 20,CreatedDate = DateTime.Now},
+                new Product() { Id = 2, CategoryId = 1, Name = "Screen", Price = 77, Stock = 2, CreatedDate = DateTime.Now },
+                new Product() { Id = 3, CategoryId = 2, Name = "LordOfTheRings", Price = 7, Stock = 5, CreatedDate = DateTime.Now},
+                new Product() { Id = 4, CategoryId = 2, Name = "RiseOfOttomanEmpire", Price = 12, Stock = 4, CreatedDate = DateTime.Now },
+                new Product() { Id = 5, CategoryId = 3, Name = "Coach", Price = 195, Stock = 11, CreatedDate = DateTime.Now },
+                new Product() { Id = 6, CategoryId = 3, Name = "Table", Price = 144, Stock = 1, CreatedDate = DateTime.Now }
+                );
+
+        }
+    }
+}
