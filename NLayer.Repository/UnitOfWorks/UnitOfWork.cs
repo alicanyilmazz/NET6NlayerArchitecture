780c680d2ec747bc0f8d8d68ba7701640288043c
alican yilmaz <alicanyilmaz101@gmail.com>
2022-02-19 17:29:03 +0300

    UnitOfWork created.

---
+++ NLayer.Repository/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLayer.Core.UnitOfWorks;
+
+namespace NLayer.Repository.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
